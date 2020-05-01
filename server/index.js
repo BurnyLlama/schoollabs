e6f5390b9c59dd3474fb80b9995193ed968975e3
BurnyLlama <burnyllama@outlook.com>
2020-05-01 21:19:13 +0200

    More work on server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 console.log('Startar servern...');
+// Import packages
+const mysql = require('mysql');
+const fs = require('fs');
+
 
 console.log('Läser in inställningar...');
-let settings;
-(async () => {
-    const settings_request = await fetch(`settings.json`);
-    settings = await settings_request.json();
-    console.log('');
-})();
 
-const mysql = require('mysql');
+const settings_file = fs.readFileSync('settings.json');
+let settings = JSON.parse(settings_file);
+
 const db = mysql.createConnection({
     host: settings.database.host,
     user: settings.database.user,
@@ -20,4 +20,3 @@ db.connect(function(err) {
     if (err) throw err;
     console.log('Anslutning till databasen är fullbordad.');
 });
-
