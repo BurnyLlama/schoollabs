972917b63d7e3911c834d6b9e11f443444a482fe
BurnyLlama <burnyllama@outlook.com>
2020-05-01 18:26:31 +0200

    More work on server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 console.log('Startar servern...');
 
+console.log('Läser in inställningar...');
+let settings;
+(async () => {
+    const settings_request = await fetch(`settings.json`);
+    settings = await settings_request.json();
+    console.log('');
+})();
+
 const mysql = require('mysql');
 const db = mysql.createConnection({
-    host: "78.72.169.7",
-    user: "schoollabs",
-    password: "schoollabs"
+    host: settings.database.host,
+    user: settings.database.user,
+    password: settings.database.pass
 });
 
 console.log('Kontaktar MySQL-servern (databas-servern)...');
